ee449cbdb0740a29e2269515df49b296de1c97fb
JasonPaulino <jasonp0830@gmail.com>
2024-06-22 00:14:49 -0400

    initial commit

---
+++ src/test/java/Spring/Boot/CRUD/API/QuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.Boot.CRUD.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
